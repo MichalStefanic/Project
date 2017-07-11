3d5b9c0b3b4ec21fb61a793dbb891098dbc7c862
MichalStefanic <mic.stefanic@gmail.com>
2017-07-11 16:47:20 +0200

    file added

--- app.py
+++ app.py
@@ -6,36 +6,44 @@ app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 @app.route("/images/upload")
 def index():
-	return render_template("upload.html")
+    return render_template("upload.html")
+
 
 @app.route("/images/uploaded", methods=['POST'])
 def upload_img():
-	target = os.path.join(APP_ROOT, "images/")
-	print(target)
+    target = os.path.join(APP_ROOT, "images/")
 
-	if not os.path.isdir(target):
-		os.mkdir(target)
-	try:
-		for file in request.files.getlist("file"):
-			filename = file.filename
-			destination = "/".join([target, filename])
-			file.save(destination)
-			svg_to_png(destination, filename)
-	except:
-		return render_template("Error.html")
-	else:
-		return get_gallery()
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    try:
+        for file in request.files.getlist("file"):
+            filename = file.filename
+            destination = "/".join([target, filename])
+            file.save(destination)
+            svg_to_png(destination, filename)
+
+    except:
+        return render_template("Error.html")
+
+    else:
+        return get_gallery()
+
 
 @app.route('/images/<filename>')
 def send_image(filename):
-	return send_from_directory('static',filename)
+    return send_from_directory('static',filename)
+
 
 @app.route('/images/gallery', methods=['POST'])
 def get_gallery():
-	images = os.listdir("./static/")
-	return render_template("gallery.html", images=images)
+    images = os.listdir("./static/")
+    return render_template("gallery.html", images=images)
+
 
 if __name__ == "__main__":
-	app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
+    
--- svg_to_png.py
+++ svg_to_png.py
@@ -11,6 +11,7 @@ def svg_to_png(path, img_name):
         png_output = os.path.join(target,img_name).split(".")[0]+".png"
         cairosvg.svg2png(
             url=path, write_to=png_output)
+        
     except:
         output_path = os.path.join(target,img_name)
         os.rename(path, output_path)
